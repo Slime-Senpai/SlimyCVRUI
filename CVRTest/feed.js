149b46d0fd477501b0685dea26ad5555c3f41c45
Slime-Senpai <44924877+Slime-Senpai@users.noreply.github.com>
2021-02-01 16:58:14 +0100

    Add Online Friends + Feed + ui.js/ui.css for Milch

---
+++ CVRTest/feed.js
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------| LINE LENGTH LIMIT
+const slimyFeed = 'Experimental 14 Changelog:\n' + // eslint-disable-line no-unused-vars
+'Game:\n' +
+'- [Props] Added a Place and Delete Mode for Props\n' +
+'- [Props] The Spawnable Component now has a configurable Spawn height\n' +
+'- [UI] Added Avatar Reload Button\n' +
+'- [UI] Add Prop Menu Button to Toolbar\n' +
+'- [General] A Portal Safe Distance can now be definded. If a portal was dropped\n' +
+'    closer than the defined Distance, it will stay disabled until You move away\n' +
+'- [General] Spamming Avatar change will no longer klog the download handler\n' +
+'- [General] The Eye Tracking of the HTC Vive Pro Eye is now supported\n' +
+'- [Avatar] CVRPointer now work with ToggleStatePointers as intended\n' +
+'- [Avatar] The Nameplate will now also light up when the Avatar has no\n' +
+'    configured Visemes\n' +
+'- [Network] Extension of the Network Package (only visible to _Users on\n' +
+'    Experimental):\n' +
+'    Animator Flying Parameter is now synced\n' +
+'    Animator Prone is now Synced\n' +
+'    Tracked Eye Movement and Eye Blinking is now synced\n' +
+'    Preperation for Synced Camera Indicator\n' +
+'- [IK] Added Option to change the Plant Feet behavior of the IK\n' +
+'- [IK] Added Option to change the Hip Following of the Tracker\n' +
+'- [Pickup] Origin Grip Mode is now Supported ingame\n' +
+'CCK:\n' +
+'- [Pickup] The CVR Pickup Component now has a gizmo which shows the Grip\n' +
+'    position and Rotation in Origin Grip Mode\n' +
+'- [World] Spawnpoint are now marked by a white Arrow\n' +
+'- [World] The Point from which the Portal Image is Taken is now marked\n' +
+'- [World] Newly Taken Portal Images schould have less Culling Artifacts\n';
