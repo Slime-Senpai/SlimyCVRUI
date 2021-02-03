33fd0f2e85bbd8161bbfa63c9ee8d6843cf1261b
Slime-Senpai <44924877+Slime-Senpai@users.noreply.github.com>
2021-02-03 15:50:04 +0100

    Experimental 15 update

--- CVRTest/feed.js
+++ CVRTest/feed.js
@@ -1,29 +1,20 @@
 // -----------------------------------------------------------------------------| LINE LENGTH LIMIT
-const slimyFeed = 'Experimental 14 Changelog:\n' + // eslint-disable-line no-unused-vars
-'Game:\n' +
-'- [Props] Added a Place and Delete Mode for Props\n' +
-'- [Props] The Spawnable Component now has a configurable Spawn height\n' +
-'- [UI] Added Avatar Reload Button\n' +
-'- [UI] Add Prop Menu Button to Toolbar\n' +
-'- [General] A Portal Safe Distance can now be definded. If a portal was dropped\n' +
-'    closer than the defined Distance, it will stay disabled until You move away\n' +
-'- [General] Spamming Avatar change will no longer klog the download handler\n' +
-'- [General] The Eye Tracking of the HTC Vive Pro Eye is now supported\n' +
-'- [Avatar] CVRPointer now work with ToggleStatePointers as intended\n' +
-'- [Avatar] The Nameplate will now also light up when the Avatar has no\n' +
-'    configured Visemes\n' +
-'- [Network] Extension of the Network Package (only visible to _Users on\n' +
-'    Experimental):\n' +
-'    Animator Flying Parameter is now synced\n' +
-'    Animator Prone is now Synced\n' +
-'    Tracked Eye Movement and Eye Blinking is now synced\n' +
-'    Preperation for Synced Camera Indicator\n' +
-'- [IK] Added Option to change the Plant Feet behavior of the IK\n' +
-'- [IK] Added Option to change the Hip Following of the Tracker\n' +
-'- [Pickup] Origin Grip Mode is now Supported ingame\n' +
-'CCK:\n' +
-'- [Pickup] The CVR Pickup Component now has a gizmo which shows the Grip\n' +
-'    position and Rotation in Origin Grip Mode\n' +
-'- [World] Spawnpoint are now marked by a white Arrow\n' +
-'- [World] The Point from which the Portal Image is Taken is now marked\n' +
-'- [World] Newly Taken Portal Images schould have less Culling Artifacts\n';
+const slimyFeed = 'Experimental 15 Changelog:\n' + // eslint-disable-line no-unused-vars
+'- [Portal] The portal safety Distance now works as intended\n' +
+'- [IK] The Foot Offset is now Properly calibrated for the forward and upward\n' +
+'    direction\n' +
+'- [IK] New Settings were added to the Experimental Setting:\n' +
+'    - Straighten Neck\n' +
+'    - Hip Shifting\n' +
+'    - Pre Straighten Spine\n' +
+'- [General] Interactables are no longer activatable while out of the defined\n' +
+'    distance\n' +
+'- [General] Disabled Visemes on Block Avatar\n' +
+'- [UI] Fixed an issue where having the Menu out of view for a long time will\n' +
+'    lead to a lag spike when the menu comes into view again\n' +
+'- [Pickups] Changed the Physics regarding Free Grab. You are now able to push\n' +
+'    the camera and other objects that are defined as free grab by pushing and\n' +
+'    re grabbing them.\n' +
+'- [Pickups] The Main collider of a pickup Object no longer collides with the\n' +
+'    Player while being held\n' +
+'- [Avatar] Visemes are now working locally again\n';
